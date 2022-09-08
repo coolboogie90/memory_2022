39d16add34cacd272bf647ffebc2cd5df778de98
CoolBoogie90 <98624859+coolboogie90@users.noreply.github.com>
2022-09-08 16:37:44 +0200

    Second animation

--- Assets/Scripts/CubeBehaviour.cs
+++ Assets/Scripts/CubeBehaviour.cs
@@ -2,18 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CubeBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
     public Material hiddenMaterial;
     public Material originalMaterial;
     public LevelManager manager;
+    private Animator animator;
     
     void Start()
     {
         originalMaterial = GetComponent<Renderer>().material;
+        animator = GetComponent<Animator>();
     }
 
+    void OnMouseEnter()
+    {
+        animator.SetBool("mouseOver", true); 
+    }
+    
+    void OnMouseExit()
+    {
+        animator.SetBool("mouseOver", false);
+    }
     void OnMouseUp()
     {
         manager.CubeIsClicked(this); //le cube dit que l'on lui a cliqué dessus et le manager va lui dire de révéler sa couleur
@@ -22,12 +34,14 @@ public class CubeBehaviour : MonoBehaviour
     public void RevealColor()
     {
         GetComponent<Renderer>().material = hiddenMaterial;
+        animator.SetBool("cubeSelected", true);
 
     }
 
     public void UnrevealColor()
     {
         GetComponent<Renderer>().material = originalMaterial;
+        animator.SetBool("cubeSelected", false);
 
     }
     // Update is called once per frame
@@ -36,13 +50,3 @@ public class CubeBehaviour : MonoBehaviour
         
     }
 }
-
-    // void OnMouseEnter() //event, nécessite un Collider
-    // {
-    //     GetComponent<Renderer>().material = hiddenMaterial;
-    // }
-
-    // void OnMouseExit()
-    // {
-    //     GetComponent<Renderer>().material = originalMaterial;
-    // }
